e7447121f62cac652c969608bb875886a43d6b1c
justice jennings <justice.l.jennings@gmail.com>
2023-11-09 12:39:08 -0500

    added fetchasync/await function and stored in created arr.

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ const apiPlayersLink = `https://fsa-puppy-bowl.herokuapp.com/api/2310-fsa-et-web
 const state = {
     players: []
 }
+// retrieve all players and info via fetch
+const getAllPlayers = async () => {
+    try {
+        const data = await fetch(apiPlayersLink)
+        const response =  await data.json()
+        state.players = response.data
+        console.log(state)
+
+        // render onto page
+    } catch(error) {
+        console.log(`There's been an error!`)
+    }
+}
+getAllPlayers();
